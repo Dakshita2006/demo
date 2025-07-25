31fb14ca99600f7e6be7e7eb013756f7482f2906
Dakshita2006 <dakkubca@gmail.com>
2025-07-25 09:25:24 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,14 @@
+class arithmatic
+{
+public static	 void main(String args[])
+
+{
+int i=20,j=10;
+System.out.println("adition of two numbers"+(i+j));
+System.out.println("subtraction of two numbers"+(i-j));
+System.out.println("multiplication of two number"+(i*j));
+System.out.println("Division of two number"+(i/j));
+System.out.println("Modulo of the number"+(i%j));
+
+  }
+}

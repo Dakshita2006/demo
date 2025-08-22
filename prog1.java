60e878ab6ee7881ec8391a8404f6076426112604
Dakshita2006 <dakkubca@gmail.com>
2025-08-22 09:58:50 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,17 @@
+import.java.util.Scanner;
+public class prog
+{
+public static void main(string args[])
+{
+float p,r,n,si;
+Scanner scan=new Scaanner(system.in);
+	system.out.println("enter the principal:");
+	 p=scan.nextFloat();
+	System.out.println("enter the rate of intrest:");
+	 r=scan.nextFloat();
+	system.out.println("enter the time period:");
+	 n=scan.nextFloat();
+	 si=(p*r*n)/ 100;
+	 system.out.println("simple interest is:"+si);
+	 }
+ }
